0d094ce0c66e92ce13b2e9a72b9b37c4dec12cf7
renukaamujuru <renuka.a@avplat.com>
2020-10-18 15:43:49 +0530

    Updates

---
+++ precache-manifest.f53e2f2456b126f8cf4f1b44ddf74673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf38f66556671706b535558e8a1c502",
+    "url": "/deploy-to-ghpages/index.html"
+  },
+  {
+    "revision": "782c8db453ae526a52b1",
+    "url": "/deploy-to-ghpages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "029dd90c16eab91b6d4d",
+    "url": "/deploy-to-ghpages/static/js/2.20689430.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/deploy-to-ghpages/static/js/2.20689430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "782c8db453ae526a52b1",
+    "url": "/deploy-to-ghpages/static/js/main.e6806b90.chunk.js"
+  },
+  {
+    "revision": "1da33b168f7bbca870d0",
+    "url": "/deploy-to-ghpages/static/js/runtime-main.62c951d2.js"
+  }
+]);
